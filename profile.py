1a301c56074426c9b1d61d2e23c519d34968c7cf
Linh B Ngo <linh.b.ngo@gmail.com>
2018-11-01 15:54:09 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -34,7 +34,9 @@ for i in range(4):
 
   node.routable_control_ip = "true"
   node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
-  
+  bs = node.Blockstore("bs" + str(i), "/hadoop")
+  bs.size = "50GB"
+    
   iface = node.addInterface("if" + str(i-3))
   iface.component_id = "eth1"
   iface.addAddress(pg.IPv4Address("192.168.1." + str(i + 1), "255.255.255.0"))
