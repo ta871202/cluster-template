21c878eac2f125cf0b6ff02a943894ceea919c0a
Linh B Ngo <linh.b.ngo@gmail.com>
2018-11-06 12:57:34 -0500

    Update profile.py

--- profile.py
+++ profile.py
@@ -31,15 +31,6 @@ for i in range(4):
   if i == 0:
     node = request.RawPC("namenode")
   else:
-<<<<<<< HEAD
-    node = request.XenVM("compute-" + str(i-2))
-    node.cores = 4
-    node.ram = 4096
-    
-  node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
-  
-  iface = node.addInterface("if" + str(i))
-=======
     node = request.RawPC("datanode-" + str(i))
 
   node.routable_control_ip = "true"
@@ -48,7 +39,6 @@ for i in range(4):
   bs.size = "50GB"
     
   iface = node.addInterface("if" + str(i-3))
->>>>>>> 364d8a06c6ef958c64046af82c89503df5596725
   iface.component_id = "eth1"
   iface.addAddress(pg.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
   link.addInterface(iface)
@@ -57,17 +47,8 @@ for i in range(4):
   node.addService(pg.Execute(shell="sh", command="sudo /local/repository/passwordless.sh"))
   node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/environment_prep.sh"))
   node.addService(pg.Execute(shell="sh", command="sudo /local/repository/environment_prep.sh"))
-  
-<<<<<<< HEAD
-  # This code segment is added per Benjamin Walker's solution to address the StrictHostKeyCheck issue of ssh
-  node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/ssh_setup.sh"))
-  node.addService(pg.Execute(shell="sh", command="sudo -H -u lngo bash -c '/local/repository/ssh_setup.sh'"))
- 
-  node.addService(pg.Execute(shell="sh", command="sudo su lngo -c 'cp /local/repository/source/* /users/lngo'"))
-=======
   if i == 0:
     node.addService(pg.Execute(shell="sh", command="sudo yum install -y ambari-server"))
->>>>>>> 364d8a06c6ef958c64046af82c89503df5596725
   
   node.addService(pg.Execute(shell="sh", command="sudo yum install -y ambari-agent"))
   node.addService(pg.Execute(shell="sh", command="sudo sed -i 's/localhost/192.168.1.1/g' /etc/ambari-agent/conf/ambari-agent.ini"))
